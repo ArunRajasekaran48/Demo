715ac2b18ba2c6fb23b08a066adf4bd595b5149a
Arun <arun.r2023cse@sece.ac.in>
2025-01-21 14:44:31 +0530

    Day 8 FN Commit

--- Day7/FrontendPractice/src/App.jsx
+++ Day7/FrontendPractice/src/App.jsx
@@ -1,20 +1,24 @@
 import ClassCompEg from "./components/ClassComponent/ClassCompEG"
 import About from "./components/FunctionalComponent/About"
-import Home from "./components/FunctionalComponent/Contact"
+import Home from "./components/FunctionalComponent/Home"
 import Gallery from "./components/FunctionalComponent/Gallery"
-import Contact from "./components/FunctionalComponent/Home"
+import Contact from "./components/FunctionalComponent/Contact"
+import Navbar from "./components/FunctionalComponent/Navbar"
+import {BrowserRouter , Routes, Route} from "react-router-dom"
 function App(){
   return(
     <div>
-      <Home/><hr/>
-      <About/><hr/>
-      <ClassCompEg /><hr/>
-      <h2>Without Props</h2>
-      <Gallery/>
-      <h2>With Props</h2>
-      <Gallery image="SECE LOGO" page="Gallery"/><hr/>
-      <Contact/>
-    </div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/gallery" element={<Gallery image="SECE LOGO" page="Gallery"/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        
+      </Routes>
+      </BrowserRouter>
+    </div> 
   )
 }
 export default App;

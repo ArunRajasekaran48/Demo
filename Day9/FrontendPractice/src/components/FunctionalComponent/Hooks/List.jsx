2fbf37a67045041a7963bdfb42f59a27de36b081
Arun <arun.r2023cse@sece.ac.in>
2025-01-24 10:39:12 +0530

    Vercel Deployment change for routing

--- Day9/FrontendPractice/src/components/FunctionalComponent/Hooks/List.jsx
+++ Day9/FrontendPractice/src/components/FunctionalComponent/Hooks/List.jsx
@@ -1,20 +1,42 @@
-import { useState, useEffect } from "react";
-
-const ListItems = ({ func }) => {
-  var [numbers, setNumbers] = useState([]);
-  useEffect(() => {
-    setNumbers(func());
-  }, [func]);
-  return (
-    <div>
-      <h3>
-        this number list as follow as according to the number present in the
-        input box
-      </h3>
-      {numbers.map((number, index) => (
-        <h4 key={index}> {number}</h4>
-      ))}
-    </div>
-  );
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../Css/Navbar.css';
+const Navbar = () => {
+    const [dropdown, showDropdown] = useState(false);
+    const toggleDropDown = () => {
+        showDropdown((prevDropdown) => !prevDropdown);
+    };
+    return (
+        <header>
+            <nav>
+                <ol>
+                    <li><Link to='/Home' className="link">Home</Link></li>
+                    <li><Link to='/About' className="link">About</Link></li>
+                    <li><Link to='/Gallery' className="link">Ga llery</Link></li>
+                    <li><Link to='/Contact' className="link">Contact</Link></li>
+                    <div 
+                        className="dropdown" 
+                        onMouseEnter={toggleDropDown} 
+                        onMouseLeave={toggleDropDown}
+                    >
+                        <div className="link">Hooks</div>
+                        {dropdown && (
+                            <ul className="dropdown-menu">
+                                <li><Link to="/usestate">UseState</Link></li>
+                                <li><Link to="/useeffect">UseEffect</Link></li>
+                                <li><Link to="/useeffectapi">UseEffectAPI</Link></li>
+                                <li><Link to="/useref">UseRef</Link></li>
+                                <li><Link to="/usereducer">UseReducer</Link></li>
+                                <li><Link to="/usememo">UseMemo</Link></li>
+                                <li><Link to="/callback">UseCallback</Link></li>
+                            </ul>
+                        )}
+                    </div>
+                    <li><Link to='/Signup' className="link">Signup</Link></li>
+                    <li><Link to='/Login' className="link">Login</Link></li>
+                </ol>
+            </nav>
+        </header>
+    );
 };
-export default ListItems;
+export default Navbar;

3a02f3ceb3df4f8997250ba3caef332c243a9156
Arun <arun.r2023cse@sece.ac.in>
2025-01-24 09:05:23 +0530

    Commiting Day10

--- Day9/FrontendPractice/src/components/FunctionalComponent/Navbar.jsx
+++ Day9/FrontendPractice/src/components/FunctionalComponent/Navbar.jsx
@@ -1,44 +1,46 @@
-import { useState } from 'react'
-import '../../Css/Navbar.css'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../Css/Navbar.css';
+
 const Navbar = () => {
-    var [dropdown, showDropdown] = useState(false)
-import { Link } from "react-router-dom"
-import "../../css/Navbar.css"
-import {useState} from "react"
-const Navbar=()=>{
-    var [dropdown,showDropdown]=useState(false)
-    const toggleDropDown=()=>{
-        showDropdown((dropdown)=>!dropdown)
-    }
-    return(
-     <header>
-         <nav>
-             <ol>
-                 <li><Link to='/Home' className="link">Home</Link></li>
-                 <li><Link to='/About' className="link">About</Link></li>
-                 <li><Link to='/Gallery' className="link">Gallery</Link></li>
-                 <li><Link to='/Contact' className="link">Contact</Link></li>
-                 <div className="dropdown">
-                        <div  className="link" onClick={(toggleDropDown)} onLeave={(toggleDropDown)}>Hooks</div>
-                        {dropdown &&(
+    const [dropdown, showDropdown] = useState(false);
+
+    const toggleDropDown = () => {
+        showDropdown((prevDropdown) => !prevDropdown);
+    };
+
+    return (
+        <header>
+            <nav>
+                <ol>
+                    <li><Link to='/Home' className="link">Home</Link></li>
+                    <li><Link to='/About' className="link">About</Link></li>
+                    <li><Link to='/Gallery' className="link">Gallery</Link></li>
+                    <li><Link to='/Contact' className="link">Contact</Link></li>
+                    <div 
+                        className="dropdown" 
+                        onMouseEnter={toggleDropDown} 
+                        onMouseLeave={toggleDropDown}
+                    >
+                        <div className="link">Hooks</div>
+                        {dropdown && (
                             <ul className="dropdown-menu">
-                                <li><Link to="/usestate" >UseState</Link></li>
+                                <li><Link to="/usestate">UseState</Link></li>
                                 <li><Link to="/useeffect">UseEffect</Link></li>
                                 <li><Link to="/useeffectapi">UseEffectAPI</Link></li>
                                 <li><Link to="/useref">UseRef</Link></li>
                                 <li><Link to="/usereducer">UseReducer</Link></li>
                                 <li><Link to="/usememo">UseMemo</Link></li>
                                 <li><Link to="/callback">UseCallback</Link></li>
-                            </ul>)}
+                            </ul>
+                        )}
                     </div>
-                 <li><Link to='/Signup' className="link">Signup</Link></li>
-                 <li><Link to='/Login' className="link">Login</Link></li>
-                 
+                    <li><Link to='/Signup' className="link">Signup</Link></li>
+                    <li><Link to='/Login' className="link">Login</Link></li>
+                </ol>
+            </nav>
+        </header>
+    );
+};
 
-             </ol>
-         </nav>
-     </header>
-    ) 
- }
- export default Navbar
+export default Navbar;
